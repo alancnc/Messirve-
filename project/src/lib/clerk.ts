b632794b7edbfa687c8a1adda093f8e9736c0898
alancnc <alan.cuenca0000@gmail.com>
2025-04-12 22:00:33 -0300

    Delete project directory

--- project/src/lib/clerk.ts
+++
@@ -1,7 +0,0 @@
-import { ClerkProvider } from '@clerk/clerk-react';
-
-export const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!clerkPubKey) {
-  throw new Error('Missing Clerk Publishable Key');
-}
